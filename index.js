4203fda73519b232a0a2d8371d45066034aab967
robertordz14 <hroberto.rdz@gmail.com>
2021-05-31 11:23:08 -0500

    prueba back

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "enrutate",
+    port: 8889
+})
+
+// app.get('/', (req, res) => {
+//     const sqlInsert = 
+//     "INSERT INTO movie_review (movieName, movieReview) VALUES ('Overboard', 'Is the best movie');";
+//     db.query(sqlInsert, (err, result) => {
+//         res.send("Datos agregados");
+//     });
+// });
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// app.get('/api/get', (req, res) => {
+//     const sqlSelect = 
+//     "SELECT * FROM movie_review ;";
+//     db.query(sqlSelect, (err, result) => {
+//         res.send(result);
+//     });
+// })
+
+
+app.get('/api/:id', (req, res) => {
+    const id = req.params.id
+    const sqlSelect = 
+    `SELECT * FROM movie_review where id = ${id};`;
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+        console.log(result);
+    });
+})
+
+app.post('/api/insert', (req, res) => {
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+    const sqlInsert = 
+    "INSERT INTO movie_review (movieName, movieReview) VALUES (?,?);";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(result);
+    });
+});
+
+ app.listen (3001, () =>{ 
+     console.log("running on port 3001");
+ });
